a8e17370f084969e4620d7d52c3b7f50076f5bbc
rita12356 <wadeew2016@yandex.ru>
2025-08-21 15:08:37 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,65 @@
+// контейнер всей игры
+let gameContainer = document.querySelector(`.game-container`);
+let hintsNode = document.querySelector('#hints');
+let textNode = document.querySelector('.text');
+
+// 4 подарка
+
+let card1 = document.querySelector(`.card1`);
+let card2 = document.querySelector(`.card2`);
+let card3 = document.querySelector(`.card3`);
+let card4 = document.querySelector(`.card4`);
+let counter = 3;
+
+// выводи сюда результат
+let alerts = document.querySelector(`#alert`);
+
+gameContainer.addEventListener(`click`, onCardClick); 
+
+
+
+function onCardClick(evt) {
+    
+    let node = evt.target;
+    
+    let card = node.closest(`.card`);
+    if(card) {
+        counter--;
+        if(counter == 2) {
+            alerts.innerHTML = `Это не то, что я хочу :( Попробуй ещё раз.`;
+            if(card.classList.contains(`card2`)) {
+             card.classList.add(`animate__flip`);
+            
+            } else{
+             card.classList.add(`animate__zoomOut`);
+            }
+         } else if(counter == 1) {
+               alerts.innerHTML = `Теперь ты знаешь, что я хочу в подарок :).`;
+            if(card.classList.contains(`card2`)) {
+             card.classList.add(`animate__flip`);
+             card1.classList.add(`animate__zoomOut`);
+             card2.classList.add(`animate__zoomOut`);
+             card3.classList.add(`animate__zoomOut`);
+             card4.classList.add(`animate__zoomOut`);
+             
+            } else{
+             card.classList.add(`animate__zoomOut`);
+            }
+
+         } else if(counter == 0) {
+            alerts.innerHTML = `Теперь ты знаешь, что я хочу в подарок :).`;
+            if(card.classList.contains(`card2`)) {
+             card.classList.add(`animate__flip`);
+           
+            } else{
+             card.classList.add(`animate__zoomOut`);
+            }
+
+         }
+     
+        }
+    }
+
+    hintsNode.addEventListener('click', function() {
+     textNode.classList.toggle('text');
+    });
